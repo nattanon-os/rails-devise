8923a3c305e2c50f3d314ba9da938c0d73090bbe
nattanon <nattanon@hospital-os.com>
2019-12-20 11:24:03 +0700

    fix for use username login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # attr_writer :login
+  # attr_accessor :login
+
+  # def login
+  #   @login || self.username || self.email
+  # end
+
+  # def self.find_for_database_authentication warden_conditions
+  #   conditions = warden_conditions.dup
+  #   login = conditions.delete(:login)
+  #   p '------------'
+  #   p login
+  #   p conditions
+  #   where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.strip.downcase}]).first
+  # end
 end
