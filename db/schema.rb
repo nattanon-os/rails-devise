b0bddc7db0207968dee5b5a09cc7588fc8f3e18e
nattanon <nattanon@hospital-os.com>
2019-12-20 10:30:36 +0700

    after run add filed username
    rails generate migration add_username_to_users username:string:uniq
    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_024132) do
+ActiveRecord::Schema.define(version: 2019_12_20_032812) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "t1"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 2019_12_20_024132) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
